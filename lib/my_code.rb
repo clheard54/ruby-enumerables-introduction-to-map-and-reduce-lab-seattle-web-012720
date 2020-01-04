6db1cd8735fcdb332fbe00358d748d435b5d91de
Clarion Heard <clarion.heard@gmail.com>
2020-01-04 18:19:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,7 +36,13 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point=0)
-    source_array.sum(starting_point)
+  total = starting_point
+  i = 0 
+  while i<source_array.length do
+      total += source_array[i]
+      i+=1 
+  end
+  total
 end
 
 def reduce_to_all_true(source_array)
