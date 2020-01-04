d0f00b08296dec4c0e8fceaf0dcf2addd5fde1e2
Clarion Heard <clarion.heard@gmail.com>
2020-01-04 18:21:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -46,7 +46,15 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.all?
+  i=0 
+  while i<source_array.length do
+    if !source_array[i]
+      return FALSE
+    else
+      i+=1 
+    end
+  end
+  return true
 end
 
 def reduce_to_any_true(source_array)
